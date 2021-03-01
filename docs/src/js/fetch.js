29fd913b60a086db292b8994f14380e740d48b07
Bryon Nicoson <b.nicoson@comcast.net>
2021-03-01 08:54:55 -0600

    Update fetch.js

--- docs/src/js/fetch.js
+++ docs/src/js/fetch.js
@@ -1,10 +1,11 @@
 const apiKey = "3VJ2XYZrTqvDfwAFbGWvvnZ5Vdfq25CqtmOn6TPveI5uveipq8";
 const apiSecret = "LS31WFEruFqRs2zmqltHcDGO3kykp7jVXfXkfjUo";
+const orgId = "IL542";
 // var petfinder = require("@petfinder/petfinder-js");
 var client = new petfinder.Client({ apiKey: apiKey, secret: apiSecret });
 
 client.animal
-  .search({ organization: "IL542" })
+  .search({ organization_id : orgId })
   .then(function (response) {
     const data = response.data;
     document.getElementById("json").textContent = JSON.stringify(data);

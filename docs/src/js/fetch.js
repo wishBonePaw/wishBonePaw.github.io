16a5fddff8a1ddc3ce6397ed36c899a5dec09b4b
Bryon Nicoson <b.nicoson@comcast.net>
2021-03-16 09:28:16 -0500

    Update fetch.js
    testing

--- docs/src/js/fetch.js
+++ docs/src/js/fetch.js
@@ -7,8 +7,27 @@ var client = new petfinder.Client({ apiKey: apiKey, secret: apiSecret });
 client.animal
   .search({ organization: orgId })
   .then(function (response) {
-    const data = response.data;
-    document.getElementById("json").textContent = JSON.stringify(data);
+    const data = response.data.animals;
+    
+    var s = $("#dogTemplate")[0].innerHTML.trim();
+    var holder = document.createElement('div);
+    holder.innerHTML = s;
+    var template = holder.childNodes;
+    var dog = document.getElementById('ourdogs');
+    data.forEach(function(object) {
+     
+      //clone template
+      var newItem = $(template).clone();
+      
+      //populate it
+      var img = $(newItem).find(".thumb")
+      $(img).attr("src", object.photos[0]).attr("alt", $(img).attr("alt") + object.name);
+      $(newItem).find(".dogname").html(object.name);
+      
+      //append it
+      $(".ourdogs").append(newItem);
+    }
+    //document.getElementById("json").textContent = JSON.stringify(data);
   })
   .catch(function (error) {
     document.getElementById("error").textContent = JSON.stringify(error);

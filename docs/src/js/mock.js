6e21d0c9c17edaa565862dd49237357fd5602d05
Bryon Nicoson <b.nicoson@comcast.net>
2021-03-16 14:38:58 -0500

    Update mock.js

--- docs/src/js/mock.js
+++ docs/src/js/mock.js
@@ -1,5 +1,7 @@
 var fr = new FileReader();
 fr.onload = function(){
+
+  fr.readAsText(./mockdata.json).then(
   document.getElementById('json').textContent = fr.result;
-  fr.readAsText(./mockdata.json);
+    )
 }

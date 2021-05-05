3cca5892992f596a227fa0b2e3dd8f6c0649f6b2
Bryon Nicoson <b.nicoson@comcast.net>
2021-05-05 17:42:29 -0500

    initial service worker

--- docs/src/js/fetch.js
+++ docs/src/js/fetch.js
@@ -1,10 +1,10 @@
 const apiKey = "3VJ2XYZrTqvDfwAFbGWvvnZ5Vdfq25CqtmOn6TPveI5uveipq8";
 const apiSecret = "LS31WFEruFqRs2zmqltHcDGO3kykp7jVXfXkfjUo";
-// var petfinder = require("@petfinder/petfinder-js");
+var petfinder = require("@petfinder/petfinder-js");
 var client = new petfinder.Client({ apiKey: apiKey, secret: apiSecret });
 
 client.animal
-  .search([{ organization_id: "IL542" }])
+  .search({organization_id: "IL542"})
   .then(function (response) {
     const data = response.data;
     document.getElementById("json").textContent = JSON.stringify(data);

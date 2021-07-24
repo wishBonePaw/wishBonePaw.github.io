6f5628f736144795eaa5337c05184ab8caa197d7
Bryon Nicoson <b.nicoson@comcast.net>
2021-07-24 16:35:02 -0500

    css positioning fixes

---
+++ docs/src/js/feed.js
@@ -0,0 +1,80 @@
+var kennel = document.querySelector("#kennel");
+var count = document.querySelector("#dog_count");
+
+//---------- testing petfinder
+//todo: obfuscation
+var key = "3VJ2XYZrTqvDfwAFbGWvvnZ5Vdfq25CqtmOn6TPveI5uveipq8";
+var secret = "LS31WFEruFqRs2zmqltHcDGO3kykp7jVXfXkfjUo";
+var org = "IL542";
+
+var animals = [];
+
+fetch("https://api.petfinder.com/v2/oauth2/token", {
+  method: "POST",
+  body:
+    "grant_type=client_credentials&client_id=" +
+    key +
+    "&client_secret=" +
+    secret,
+  headers: {
+    "Content-type": "application/x-www-form-urlencoded",
+  },
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("[TOKEN]", data);
+
+    return fetch(
+      "https://api.petfinder.com/v2/animals?limit=100&organization=" + org,
+      {
+        headers: {
+          Authorization: data.token_type + " " + data.access_token,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log("[ANIMALS]", data);
+        //console.log("[visualViewport.width]", visualViewport.width);
+        animals = data.animals;
+        count.innerHTML = animals.length;
+        animals.sort(function (a, b) {
+          return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
+        });
+        animals.forEach(createCard);
+      })
+      .catch(function (err) {
+        console.log("[ERROR]", err);
+      });
+  });
+
+function createCard(dog, index, array) {
+  var cardWrapper = document.createElement("div");
+  cardWrapper.className = "mdl-cell mdl-cell--2-col";
+  // (index + 1) % 5 != 0
+  //   ? cardWrapper.className = 'mdl-cell mdl-cell--2-col mdl-cell--2-row'
+  //   : cardWrapper.className = 'mdl-cell mdl-cell--4-col mdl-cell--4-row';
+
+  var cardPhoto = document.createElement("img");
+  cardPhoto.className = "img-responsive";
+  cardPhoto.src = dog.primary_photo_cropped.small;
+  cardPhoto.style.width = "100%";
+  cardPhoto.style.borderRadius = "15px";
+  cardWrapper.appendChild(cardPhoto);
+
+  var cardTitle = document.createElement("div");
+  cardTitle.style.alignmentBaseline = "baseline";
+  cardTitle.style.textAlign = "center";
+  cardTitle.className = "col mdl-typography--title";
+  cardTitle.appendChild(document.createTextNode(dog.name));
+
+  cardWrapper.appendChild(cardTitle);
+
+  componentHandler.upgradeElement(cardWrapper);
+  kennel.appendChild(cardWrapper);
+}
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,9 @@
 self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing service worker ...", event);
+  caches.open("static").then(function (cache) {
+    console.log("[Service Worker] Precaching App Shell");
+    cache.add("/src/js/app.js");
+  });
 });
 
 self.addEventListener("activate", function (event) {
